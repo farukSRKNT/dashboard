07d4967e0f1d9d2a2c3e8cab6446c2519c0a5534
Faruk <frk.srknt@gmail.com>
2021-08-08 00:15:12 +0300

    impl top movies

---
+++ src/components/TopMovies/styles.js
@@ -0,0 +1,41 @@
+import { makeStyles } from "@material-ui/core";
+
+
+export default makeStyles((theme) => ({
+    moviesWrapper: {
+        width: '363px',
+        padding: '30px 23px'
+    },
+    title: {
+        fontSize: '20px',
+        marginBottom: '2px'
+    },
+    movie: {
+        display: 'flex',
+        fontSize: '15px',
+        alignItems: 'center',
+        marginTop: '18px',
+        '&:last-child $movieInfoWrapper': {
+            borderBottom: 'none'
+        }
+    },
+    movieInfoWrapper: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexGrow: '1',
+        marginRight: '23px',
+        marginLeft: '18px',
+        height: '35px',
+        borderBottom: '1px solid #DDE9FB'
+    },
+    movieName: {
+        color: '#4C8DEB'
+    },
+    scoreWrapper: {
+        display: 'flex'
+    },
+    score: {
+        marginLeft: '4px'
+    }
+  }))
