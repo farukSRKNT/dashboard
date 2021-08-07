cec5ea6b0d96e16ed8acf9a40ace10100ffe30f3
Faruk <frk.srknt@gmail.com>
2021-08-08 00:17:35 +0300

    fix imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles'
 import Categories from '../components/Categories/Categories'
-import WatchTime from '../components/WatchTime'
-import TopMovies from '../components/TopMovies'
-import Movies from '../components/Movies'
-import Activity from '../components/Activity'
+import WatchTime from '../components/WatchTime/WatchTime'
+import TopMovies from '../components/TopMovies/TopMovies'
+import Movies from '../components/MovieSearch/MovieSearch'
+import Activity from '../components/Activity/Activity'
 
 const useStyles = makeStyles((theme) => ({
     row: {
