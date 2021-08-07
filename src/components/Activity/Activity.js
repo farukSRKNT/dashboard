8d24ba33a9ea8eea0b5babc6db033a1df0f4d12e
Faruk <frk.srknt@gmail.com>
2021-08-08 00:13:23 +0300

    implement Activity

---
+++ src/components/Activity/Activity.js
@@ -0,0 +1,67 @@
+import React from "react"
+import ActivityItem from "./ActivityItem"
+import { makeStyles } from "@material-ui/core"
+
+const data = [
+    {
+        time: '11.03AM',
+        date: '11-02-2028',
+        activityType: '1',
+        content: 'Invoice #0075 of amount $2000 has been emailed to email@address.com'
+    },
+    {
+        time: '11.03AM',
+        date: '11-02-2028',
+        activityType: '2',
+        content: 'Invoice #0072 of amount $1300 has been paid and added to your account.'
+    },
+    {
+        time: '11.03AM',
+        date: '11-02-2028',
+        activityType: '3',
+        content: 'Invoice #0076 of amount $1300 has been generated and is ready to be sent.'
+    },
+    {
+        time: '11.03AM',
+        date: '11-02-2028',
+        activityType: '1',
+        content: 'Invoice #0075 of amount $2000 has been emailed to email@address.com'
+    },
+    {
+        time: '11.03AM',
+        date: '11-02-2028',
+        activityType: '2',
+        content: 'Invoice #0072 of amount $1300 has been paid and added to your account.'
+    },
+    {
+        time: '11.03AM',
+        date: '11-02-2028',
+        activityType: '3',
+        content: 'Invoice #0076 of amount $1300 has been generated and is ready to be sent.'
+    },
+]
+
+const useStyles = makeStyles((theme) => ({
+    activityListTitle: {
+        fontSize: '20px',
+        marginBottom: '20px'
+    },
+    activityList: {
+        padding: '29px 17px 17px'
+    }
+}))
+
+export default function Activity(Props) {
+    const classes = useStyles()
+
+    return (
+        <div>
+            <div className={classes.activityListTitle} >Activty</div>
+            <div className={classes.activityList}>
+                {data.map((activity, index) => (
+                    <ActivityItem key={`activity-${index}`} activity={activity}/>
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Activity/ActivityItem.js
@@ -0,0 +1,88 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core"
+import SentIcon from '../../assets/icons/sent.svg'
+import ReceivedIcon from '../../assets/icons/received.svg'
+import GeneratedIcon from '../../assets/icons/generated.svg'
+
+const icons = {
+    '1': SentIcon,
+    '2': ReceivedIcon,
+    '3': GeneratedIcon
+}
+
+const useStyles = makeStyles((theme) => ({
+    activityItem: {
+        display: 'flex',
+        height: '115px'
+    },
+    dateTime: {
+
+    },
+    time: {
+        fontSize: '8px',
+    },
+    date: {
+        fontSize: '10px'
+    },
+    iconLine: {
+        position: 'relative',
+        margin: '0 7px'
+    },
+    iconWrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '24px',
+        height: '24px',
+        borderRadius: '25px',
+        boxShadow: '0px 2px 4px rgba(76, 141, 235, 0.256992)',
+        zIndex: '1'
+    },
+    line: {
+        position: 'absolute',
+        width: '12px',
+        height: '100%',
+        top: 0,
+        right: 0,
+        borderLeft: '1px solid rgba(76, 141, 235, 0.186481)'
+    },
+    messageBox: {
+        border: '1px solid #DDE9FB',
+        borderRadius: '5px',
+        width: '205px',
+        height: 'fit-content',
+        padding: '10px 10px 12px'
+    },
+    title: {
+
+    },
+    content: {
+    }
+}))
+
+export default function Activity(props) {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.activityItem}>
+            <div className={classes.dateTime}>
+                <div className={classes.time}>{props.activity.time}</div>
+                <div className={classes.date}>{props.activity.date}</div> 
+            </div>
+            <div className={classes.iconLine}>
+                <div className={classes.iconWrapper}>
+                    <img src={icons[props.activity.activityType]} width={13} height={12} alt="activity type"/>
+                </div>
+                <div className={classes.line}></div> 
+            </div>
+            <div className={classes.messageBox}>
+                <div className={classes.title}>
+                    {props.activity.title}
+                </div>
+                <div className={classes.content}>
+                    {props.activity.content}
+                </div>
+            </div>
+        </div>
+    )
+}
