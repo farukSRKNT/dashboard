b43edd8b33b5bb729982fd3ff2940f7ba88adcdc
Faruk <frk.srknt@gmail.com>
2021-08-15 01:25:11 +0300

    add custom label

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { makeStyles } from "@material-ui/core";
+import {useSelector, useDispatch} from 'react-redux'
+import {changeCategoryInterval} from '../../actions'
 
 import {
     ResponsiveContainer,
     PieChart,
     Pie,
     Cell,
-    Label
+    Sector
   } from "recharts";
 
 import SimpleSelect from '../SimpleSelect'
 
 const useStyles = makeStyles((theme) => ({
   categoriesWrapper: {
-    width: '360px',
+    width: '40%',
     height: '351px',
     marginRight: '17px',
     marginBottom: '17px'
@@ -21,6 +23,13 @@ const useStyles = makeStyles((theme) => ({
   upperBar: {
     display: 'flex',
     justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: '20px'
+  },
+  responsiveContainer: {
+    margin: 'auto'
   },
   categoriesColorsWrapper: {
     display: 'flex',
@@ -42,45 +51,79 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const PieChartData = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
-
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const SELECT_OPTIONS = [
+  {value: 'WEEK', name: 'This Week'},
+  {value: 'MONTH', name: 'This Month'},
+  {value: 'YEAR', name: 'This Year'}
+]
+
+const renderActiveShape = (props, movieCount) => {
+  console.log(movieCount)
+  const { cx, cy, innerRadius, outerRadius, startAngle, endAngle, fill } = props;
+
+  return (
+    <g>
+      <text x={cx} y={cy-10} dy={8} textAnchor="middle" fontWeight="bold" fill="#405169">
+        {movieCount}
+      </text>
+      <text x={cx} y={cy+10} dy={8} textAnchor="middle" fill="#A0BCE4">
+        movies
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+    </g>
+  );
+};
 
 export default function Categories(props) {
   const classes = useStyles();
+  const categories = useSelector(state => state.categories)
+  const categoriesMovieCount = useSelector(state => state.categoriesMovieCount)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(changeCategoryInterval(SELECT_OPTIONS[0].value))
+  }, [])
+
+  const handleChange = (value) => {
+    dispatch(changeCategoryInterval(value))
+  };
 
 return (
   <div className={classes.categoriesWrapper}>
     <div className={classes.upperBar}>
-      <div>Categories</div>
-      <SimpleSelect />
+      <div className={classes.title}>Categories</div>
+      <SimpleSelect options={SELECT_OPTIONS} handleChange={handleChange}/>
     </div>
-    <ResponsiveContainer width={204} height={204}>
-      <PieChart>
+    <ResponsiveContainer width={204} height={264} className={classes.responsiveContainer}>
+      <PieChart >
         <Pie
-          data={PieChartData}
-          innerRadius={80}
+          activeIndex={0}
+          data={categories}
+          activeShape={(defaultProps) => renderActiveShape(defaultProps, categoriesMovieCount)}
+          innerRadius={75}
           outerRadius={90}
           dataKey="value"
         >
-        <Label value="20148 Movies" position="center" />
-          {PieChartData.map((entry, index) => (
+          {categories.map((entry, index) => (
             <Cell
               key={`cell-${index}`}
               fill={COLORS[index % COLORS.length]}
             />
           ))}
         </Pie>
-        <div>Text</div>
       </PieChart>
     </ResponsiveContainer>
     <div className={classes.colorWrapper}>
-      {PieChartData.map((entry, index) => (
+      {categories.map((entry, index) => (
           <div key={`color-${index}`} className={classes.category}>
                 <div
                   className={classes.colorSquare}  
