b9299017b05232a772446a9ad4085a533728a7f1
Faruk <frk.srknt@gmail.com>
2021-08-15 20:43:26 +0300

    fix: comment out redundant theme overrides

--- src/theme.js
+++ src/theme.js
@@ -2,31 +2,31 @@ import { createTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
     overrides: {
-        Mui: {
-            focused: {
-                backgroundColor: 'white'
-            }
-        },
-        MuiInput: {
-            underline: {
-                '&::before': {
-                    display: 'none'
-                },
-                '&::focused': {
-                    transform: 'ScaleX(0)'
-                }
-            },
-            focused: {
-                '&::before': {
-                    display: 'none'
-                }
-            }
-        },
-        MuiSelect: {
-            '&::focus': {
-                backgroundColor: 'white'
-            }
-        }
+        // Mui: {
+        //     focused: {
+        //         backgroundColor: 'white'
+        //     }
+        // },
+        // MuiInput: {
+        //     underline: {
+        //         '&::before': {
+        //             display: 'none'
+        //         },
+        //         '&::focused': {
+        //             transform: 'ScaleX(0)'
+        //         }
+        //     },
+        //     focused: {
+        //         '&::before': {
+        //             display: 'none'
+        //         }
+        //     }
+        // },
+        // MuiSelect: {
+        //     '&::focus': {
+        //         backgroundColor: 'white'
+        //     }
+        // }
     },
 });
 
