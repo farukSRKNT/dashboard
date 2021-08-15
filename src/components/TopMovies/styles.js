e7e2bdcf30776f439caf06cb6ef396abecb321bd
Faruk <frk.srknt@gmail.com>
2021-08-15 20:39:13 +0300

    fix: css

--- src/components/TopMovies/styles.js
+++ src/components/TopMovies/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core";
 
 export default makeStyles((theme) => ({
     moviesWrapper: {
-        width: '40%',
+        width: 'calc(100% - 46px)',
         padding: '30px 23px'
     },
     title: {
