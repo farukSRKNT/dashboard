7b987d76015f8b7243e23f7a7ab381d45f9b5976
Faruk <frk.srknt@gmail.com>
2021-08-15 20:40:47 +0300

    fix: get style using props

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import { InputBase, } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
-    search: {
+    search: props => ({
         border: '1px solid #A0BCE4',
         position: 'relative',
         height: '30px',
@@ -13,14 +13,14 @@ const useStyles = makeStyles((theme) => ({
         '&:hover': {
           backgroundColor: alpha(theme.palette.common.white, 0.25),
         },
-        marginRight: theme.spacing(2),
+        marginRight: props.marginRight || theme.spacing(2),
         marginLeft: 0,
         // width: '82px',
         [theme.breakpoints.up('sm')]: {
-          marginLeft: theme.spacing(3),
+          // marginLeft: theme.spacing(3),
           width: 'auto',
         },
-      },
+      }),
       searchIcon: {
         padding: '0 10px',
         height: '100%',
