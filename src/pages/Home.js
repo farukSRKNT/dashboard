ca5232fa2916a2f83651a67da58d6f1aa707b98c
Faruk <frk.srknt@gmail.com>
2021-08-15 20:42:09 +0300

    fix: use grid to make page responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles'
+import { Grid } from '@material-ui/core'
 import Categories from '../components/Categories/Categories'
 import WatchTime from '../components/WatchTime/WatchTime'
 import TopMovies from '../components/TopMovies/TopMovies'
@@ -7,18 +8,22 @@ import Activity from '../components/Activity/Activity'
 
 const useStyles = makeStyles((theme) => ({
     row: {
-        display: 'flex'
+        display: 'flex',
+    },
+    title: {
+        fontSize: '28px',
+        color: '#405169',
+        fontWeight: '600'
     }
   }))
-
 
 export default function Home(Props) {
   const classes = useStyles();
 
     return (
         <>
-            <h1>Dashboard</h1>
-            <div className={classes.row}>
+            <div className={classes.title}>Dashboard</div>
+            {/* <div className={classes.row}>
                 <div style={{display: 'block'}}>
                     <div className={classes.row}>
                     <Categories />
@@ -29,8 +34,27 @@ export default function Home(Props) {
                     <Movies />
                     </div>
                 </div>
-                <Activity />
-            </div>
+                <Activity /> */}
+            {/* </div> */}
+            <Grid container xs={12}>
+                <Grid xs={12} sm={6} md={9} item container>
+                    <Grid item xs={12} md={5}>
+                    <Categories />
+                    </Grid>
+                    <Grid item xs={12}  md={7}>
+                    <WatchTime />
+                    </Grid>
+                    <Grid item  xs={12} md={5}>
+                    <TopMovies />
+                    </Grid>
+                    <Grid item xs={12} md={7}>
+                    <Movies />
+                    </Grid>
+                </Grid>
+                <Grid item container xs={12} sm={6} md={3}  >
+                    <Activity />
+                </Grid>
+            </Grid>
         </>
     )
 }
